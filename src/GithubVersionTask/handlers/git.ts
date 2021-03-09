10b96483fda0597ab8b08184078840be3c86b287
Jared-Lotti <jared_lotti@carmax.com>
2021-03-09 15:57:49 -0500

    round 2

--- src/GithubVersionTask/handlers/git.ts
+++ src/GithubVersionTask/handlers/git.ts
@@ -1,7 +1,48 @@
-import SimpleGit from 'simple-git';
+import SimpleGit from "simple-git";
 
 const git = SimpleGit();
 
-export const GetRemoteDetails = () => {
-  git.listConfig()
-}
+export const GetRemoteOwnerAndRepository = async () => {
+  const config = await git.listConfig();
+  const remoteUri = config.values[".git/config"]["remote.origin.url"] as string;
+
+  const regExMatches = remoteUri.match(
+    /[:|\/]([-_0-9a-zA-Z]+)\/([-_0-9a-zA-Z]+).git/
+  );
+
+  if (regExMatches == null) {
+    throw new Error("Failed to parse owner and repository from remote origin");
+  }
+  console.debug("stuff", remoteUri);
+  const owner = regExMatches[1];
+  const repository = regExMatches[2];
+
+  return { owner, repository };
+};
+
+export const GetGithubApiUri = async () => {
+  const config = await git.listConfig();
+  const remoteUri = config.values[".git/config"]["remote.origin.url"] as string;
+
+  const regExMatches = remoteUri.match(/[:|\/@]([-_0-9a-zA-Z.]+).*.git/);
+
+  if (regExMatches == null) {
+    throw new Error("Failed to parse api uri from remote origin");
+  }
+
+  const domain = regExMatches[1];
+
+  if (domain.toLowerCase() === "github.com") {
+    return "https://api.github.com";
+  } else {
+    return `https://${domain}/api/v3`;
+  }
+};
+
+export const GetCurrentBranch = async () => {
+  return git.revparse(["--abbrev-ref", "HEAD"]);
+};
+
+export const GetCurrentCommit = async () => {
+  return git.revparse(["HEAD"]);
+};
--- src/GithubVersionTask/index.ts
+++ src/GithubVersionTask/index.ts
@@ -1,9 +1,26 @@
 import * as task from "azure-pipelines-task-lib/task";
 
-async function run() {
-  const token = task.getInput("GitHubConnection", true);
+import {
+  GetRemoteOwnerAndRepository,
+  GetCurrentBranch,
+  GetCurrentCommit,
+  GetGithubApiUri,
+} from "./handlers/git";
 
-  console.log("Selected Token", token);
+import { AuthorizeGithubConnection } from "./handlers/devops";
+
+async function run() {
+  const githubConnection = task.getInput("GitHubConnection", true) as string;
+
+  console.log("Selected Token", githubConnection);
+  console.log("Remote", await GetRemoteOwnerAndRepository());
+  console.log("Current Branch", await GetCurrentBranch());
+  console.log("Current Commit", await GetCurrentCommit());
+  console.log("Remote Api Uri", await GetGithubApiUri());
+  console.log(
+    "Github Connection Token",
+    AuthorizeGithubConnection(githubConnection)
+  );
 }
 
 run().catch((error) => task.setResult(task.TaskResult.Failed, error.message));
